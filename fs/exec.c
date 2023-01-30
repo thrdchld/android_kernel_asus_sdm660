c049222e6ee9bfbeabfbc5670942cfcf253b6c20
Vhmit <viktor.9630@hotmail.com>
2023-01-30 17:11:24 +0800

    KSU: Import KernelSU for linux 4.4
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1527,6 +1527,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			       void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1542,6 +1547,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 	bool is_su;
 
+        ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
